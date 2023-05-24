dc1221968726719da8f7aec9aad54c3485037fba
AlexandraaMart <martynovaalex049@gmail.com>
2023-05-24 23:17:51 +0300

    add

--- JS3/lab3.js
+++ JS3/lab3.js
@@ -1,21 +1,14 @@
 export function getDecimal(num) {
-    if (Math.trunc(num) == num) {
-        return 0;
-    }
-    else if (num < 0) {
-        return num - (-Math.ceil(-num));
-    }
-    if (num > 1) {
-        return +(num - Math.trunc(num)).toFixed(2);
-    }
+    if (Math.trunc(num) == num) return 0;
+    else if (num < 0) return num - (-Math.ceil(-num));
+    if (num > 1) return +(num - Math.trunc(num)).toFixed(2);
+
 }
 
 
 
 export function ucFirst(str) {
-    if (!str) {
-        return str;
-    }
+    if (!str) return str;
     else {
         let NewStr = str[0].toUpperCase() + str.slice(1);
         return NewStr;
@@ -32,12 +25,8 @@ export function checkSpam(str) {
 
 
 export function truncate(str, maxlength) {
-    if (str.length < maxlength) {
-        return str;
-    }
-    else {
-        return str.slice(0, maxlength - 1) + '…';
-    }
+    if (str.length < maxlength) return str;
+    else return str.slice(0, maxlength - 1) + '…';
 }
 
 
@@ -46,34 +35,28 @@ export function camelize(str) {
     let STR = str.split("-");
     let StrNew = "";
     for (let i = 0; i <= STR.length - 1; i++) {
-        if (i == 0) {
-            StrNew = STR[0];
-        }
+        if (i == 0) StrNew = STR[0];
+
         else StrNew += ucFirst(STR[i]);
     }
     return StrNew;
 }
 
-export function fib(n) {
-    let a = 1n;
-    let b = 1n;
-    if (n == 0n) {
-        return 0n;
-    }
-    else {
-        for (let i = 3n; i <= n; i++) {
-            let c = a + b;
-            a = b;
-            b = c;
-        }
-        return b;
-    }
+export function fibs(n) {
+    let arr = [];
+    for (let i = 0; i < n; i++) arr.push(fib(i));
+    return arr;
 }
 
-export function fibs(n){
-    let arr = [];
-    for (let i = 0; i<n; i++){
-        arr.push(fib(i));
-    }
-    return arr;
+export function arrReverseSorted(arr) {
+    return arr.sort(function (a, b) {
+        return b - a;
+    });
+}
+
+
+export function unique(arr) {
+    let set = new Set();
+    for (let i in arr) set.add(arr[i]);
+    return Array.from(set);
 }
